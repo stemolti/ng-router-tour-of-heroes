7ef80105639e35aa52695a47ba02b7dae023457c
Stefano Agbodan <mystefano95@gmail.com>
2024-05-10 09:37:30 +0200

    Initial setup, Wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes =[
+  {path: 'crisis-center', component: CrisisListComponent},
+  {path: 'heroes', component: HeroListComponent},
+  {path: '', redirectTo:'/heroes', pathMatch:'full'},
+  {path: '**', component: PageNotFoundComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(
+        routes, {enableTracing: true}  // <-- debugging purposes only
+        )
+  ],
+  exports:[RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
